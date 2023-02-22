15aadaf91e0b771e3ce674e0a1f6bd66f58a0c46
Kirill Kirsanov <montego@yandex.ru>
2023-02-22 16:50:29 +0300

    Added base functionality

--- main/models.py
+++ main/models.py
@@ -6,8 +6,14 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class MenuItem(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children')
+
+    def __str__(self):
+        return f'{self.menu.name} - {self.name} - child of {self.parent.name}'
--- main/templatetags/draw_menu.py
+++ main/templatetags/draw_menu.py
@@ -1,3 +1,11 @@
 from django import template
+from ..models import Menu, MenuItem
 
 register = template.Library()
+
+
+@register.inclusion_tag('menu.html')
+def draw_menu(menu_name):
+    menu = Menu.objects.get(name=menu_name)
+    menu_items = MenuItem.objects.filter(menu=menu, parent=None).order_by('id')
+    return {'menu_items': menu_items}
